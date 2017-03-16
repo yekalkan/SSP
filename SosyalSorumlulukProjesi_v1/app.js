900b20a9d0159b6c22e024ac9ec8603c87db431b
yekalkan <yunus.kalkan@st.cs.deu.edu.tr>
2017-03-16 14:16:12 +0300

    signup added.

--- SosyalSorumlulukProjesi_v1/app.js
+++ SosyalSorumlulukProjesi_v1/app.js
@@ -13,6 +13,7 @@ var db = monk('localhost:27017/SSPdatabase');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+var signup = require('./routes/signup');
 var mainpage = require('./routes/mainpage');
 
 var app = express();
@@ -39,27 +40,47 @@ app.use(function(req,res,next){
 app.use('/', index);
 app.use('/users', users);
 app.use('/login', login);
+app.use('/signup', signup);
 app.use('/mainpage', mainpage);
 
 
 
 app.post('/loginprovider',function(req,res){
-    var user_name=req.body.username.trim();
+    var e_mail=req.body.email.trim();
     var password=req.body.password;
 
     var db = req.db;
     var users = db.get('users');
-    users.find({"username":user_name,"password":password}, function(err, result) {
+    users.find({"email":e_mail,"password":password}, function(err, result) {
         if (err) throw err;
         console.log(result);
 
         if(result.length > 0){
-            req.session.username = user_name;
+            req.session.username = e_mail;
             res.redirect('/mainpage');
         }
         else{
             res.redirect('/login');
         }
+    });
+});
+
+app.post('/adduSer',function(req,res){
+    var e_mail=req.body.email.trim();
+    var password=req.body.password;
+    var password2=req.body.password2;
+
+    if (password != password2 || e_mail === "" || password ===""){
+        res.redirect('/signup');
+    }
+
+    var db = req.db;
+    var users = db.get('users');
+    users.insert({"email":e_mail,"password":password}, function(err, result) {
+        if (err) throw err;
+
+            req.session.username = e_mail;
+            res.redirect('/mainpage');
     });
 });
 
