d5e123adbcf78b82953105f8c62495517f47e0af
yekalkan <yunus.kalkan@st.cs.deu.edu.tr>
2017-05-25 14:10:40 +0300

    .... 25 mayıs

---
+++ SosyalSorumlulukProjesi_v1/routes/haziresyalar.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    if(req.session.email == undefined){
+        res.redirect('/');
+    }
+    else if(req.session.usertype === "temsilci"){
+        if(req.session.infoneeded){
+            res.redirect('/temsilcibilgileri');
+        }
+        else{
+            res.render('haziresyalar', { username: req.session.email});
+        }
+    }
+    else if(req.session.usertype === "bagisci"){
+        res.render('haziresyalar', { username: req.session.email});
+    }
+    else if(req.session.usertype === "admin"){
+        res.render('haziresyalar', { username: req.session.email});
+    }
+
+});
+
+module.exports = router;
